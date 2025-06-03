f294ab0b4d59b83c9aae75a499d15b94b439adb2
Dar00019 <155844356+Dar00019@users.noreply.github.com>
2025-06-04 00:54:37 +0500

    start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-pip install -r requirements.txt
-python main.py
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
+export FLASK_APP=main.py
+python3 main.py
